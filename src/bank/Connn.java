c2df2edd9e293dda2399e8cd8ee90ff8c60ed0eb
Purti21 <168754352+Purti21@users.noreply.github.com>
2025-06-16 17:31:24 +0530

    Add files via upload

---
+++ src/bank/Connn.java
@@ -0,0 +1,20 @@
+package bank;
+
+import java.sql.*;
+
+public class Connn {
+    Connection connection;
+    Statement statement;
+    public Connn(){
+        try{
+        	Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankmanagement","root","purti123");
+            statement = connection.createStatement();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+}
